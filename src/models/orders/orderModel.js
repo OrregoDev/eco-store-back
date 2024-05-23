dce666301d77c67af518d4606779ad3355f76f46
OrregoDev <camilo.orrego554@gmail.com>
2024-05-23 17:47:17 -0500

    create update and delete order model

--- src/models/orders/orderModel.js
+++ src/models/orders/orderModel.js
@@ -1,15 +1,15 @@
-const { pool: poolmysql } = require("../../config/database");
+const { pool: poolmysql } = require('../../config/database');
 
 const createOrder = ({ id_user, product_id, payment_method, address }) => {
   const query =
-    "INSERT INTO orders (id_user, product_id, payment_method, address) VALUES (?, ?, ?, ?)";
+    'INSERT INTO orders (id_user, product_id, payment_method, address) VALUES (?, ?, ?, ?)';
   return new Promise((resolve, reject) => {
     poolmysql.query(
       query,
       [id_user, product_id, payment_method, address],
-      (err, result, fields) => {
+      (err, result) => {
         if (err) {
-          console.error("Error from userModel.js", { err });
+          console.error('Error from userModel.js', { err });
           return reject(err);
         }
         resolve(result.insertId);
@@ -18,6 +18,45 @@ const createOrder = ({ id_user, product_id, payment_method, address }) => {
   });
 };
 
+const updateOrder = ({
+  orderId,
+  id_user,
+  product_id,
+  payment_method,
+  address,
+}) => {
+  const query =
+    'UPDATE orders SET id_user =?, product_id =?, payment_method =?, address =? WHERE OrderID =?';
+  return new Promise((resolve, reject) => {
+    poolmysql.query(
+      query,
+      [id_user, product_id, payment_method, address, orderId],
+      (err, result) => {
+        if (err) {
+          console.error('Error from userModel.js', { err });
+          return reject(err);
+        }
+        resolve(result.affectedRows > 0);
+      }
+    );
+  });
+};
+
+const deleteOrder = (orderId) => {
+  const query = 'DELETE FROM orders WHERE OrderID =?';
+  return new Promise((resolve, reject) => {
+    poolmysql.query(query, [orderId], (err, result) => {
+      if (err) {
+        console.error('Error from userModel.js', { err });
+        return reject(err);
+      }
+      resolve(result.affectedRows > 0); // Retorna true si se eliminó al menos una fila
+    });
+  });
+};
+
 module.exports = {
   createOrder,
+  updateOrder,
+  deleteOrder,
 };
